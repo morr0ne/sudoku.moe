a0f080fe1f9fc379ece2da9ef4ba8628cf6577b6
Matilde Morrone <contact@morrone.dev>
2025-11-13 11:15:27 +0100

    Configure for dynamic rendering

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import solidJs from '@astrojs/solid-js';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	site: 'https://sudoku.moe',
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
 	integrations: [solidJs()],
 	vite: {
 		plugins: [tailwindcss()],
